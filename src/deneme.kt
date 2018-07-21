37076ba607998bb14f1511948fc9c228c7eab419
İnci <incidemirel3@gmail.com>
2018-07-21 13:13:39 +0300

    GFTYR

---
+++ src/deneme.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    var name = "Bu benim evim"
+    var home = """
+
+
+
+                   *
+                *     *
+              *    *    *
+              *         *
+              *    *    *
+
+              $name
+
+"""
+
+    println(home)
+
+
+
+}
